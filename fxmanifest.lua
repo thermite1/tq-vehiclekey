cade8dc4fb520086cc556c686526775431c5f346
Berkay <71380426+thermite1@users.noreply.github.com>
2022-01-05 19:03:07 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,9 @@ game 'gta5'
 name 'vehicle lock system like a 8born'
 author 'tq_store'
 
-server_script 'server.lua'
+server_scripts {	
+  '@mysql-async/lib/MySQL.lua',
+  'server.lua'
+}
+
 client_script 'client.lua'
